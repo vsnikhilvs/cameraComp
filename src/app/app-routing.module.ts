f3638b6a920d70b2cb0b4691428373ae7d7ac570
Nikhil V S <vsnikhilvs@gmail.com>
2020-11-14 22:36:33 +0530

    Seconf commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import {FormsModule} from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {PickListModule} from 'primeng/picklist';
+import {DialogModule} from 'primeng/dialog';
+import {TableModule} from 'primeng/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ButtonModule,
+    DropdownModule,
+    FormsModule,
+    PickListModule,
+    ButtonModule,
+    DialogModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
